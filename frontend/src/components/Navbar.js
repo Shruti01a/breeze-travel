52d78daa8e6822ab4c2b896028c834e80f8b587c
Shruti Mishra <shruti8434a@gmail.com>
2026-04-20 02:56:02 +0530

    Change default avatar character to emoji

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
               <line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="18" x2="21" y2="18"/>
             </svg>
             <div className="avatar-circle">
-              {user ? user.name[0].toUpperCase() : '?'}
+              {user ? user.name[0].toUpperCase() : '🤔'}
             </div>
           </div>
           {menu && (
